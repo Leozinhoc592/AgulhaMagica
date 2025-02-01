3c34a4562275f3631a0e8ac9c3ef5b436f283f6a
Leo de Rezende Câmara <114754618+Leozinhoc592@users.noreply.github.com>
2025-02-01 00:30:07 -0300

    menu lateral funcional

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-document.querySelector("#item-1").addEventListener("click", function() {
-    console.log("teste");
-});
+
+    document.addEventListener("DOMContentLoaded", function () {
+        let carrossel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'));
+
+        document.getElementById("item-1").addEventListener("click", function () {
+            carrossel.to(0);
+        });
+
+        document.getElementById("item-2").addEventListener("click", function () {
+            carrossel.to(1);
+        });
+
+        document.getElementById("item-3").addEventListener("click", function () {
+            carrossel.to(2);
+        });
+    });

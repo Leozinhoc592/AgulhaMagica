fb1dc5033d7bd7a094da0a617a7b8d7ddf0b62a2
Leo Camara <leozinhoc592@gmail.com>
2025-02-01 14:07:30 -0300

    Organização de menu
    Organizei os menus para ficarem menos desalinhados.

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
     document.addEventListener("DOMContentLoaded", function () {
         let carrossel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'));
 
-        document.getElementById("item-1").addEventListener("click", function () {
+        document.getElementById("item-1").addEventListener("mouseover", function () {
             carrossel.to(0);
         });
 
-        document.getElementById("item-2").addEventListener("click", function () {
+        document.getElementById("item-2").addEventListener("mouseover", function () {
             carrossel.to(1);
         });
 
-        document.getElementById("item-3").addEventListener("click", function () {
+        document.getElementById("item-3").addEventListener("mouseover", function () {
             carrossel.to(2);
         });
     });
